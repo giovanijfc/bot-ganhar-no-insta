8079528a76e12360fd61254653e6ed6760ef20c5
unknown <giovanijfc@gmail.com>
2020-10-11 21:07:14 -0300

    fix: adds interval to account blocked

--- src/main/java/bot/instagram/GanharNoInsta.java
+++ src/main/java/bot/instagram/GanharNoInsta.java
@@ -41,7 +41,7 @@ public class GanharNoInsta {
 				if (account.getLastDateFollowFlow() == null) {
 					accountFollowFlow(account);
 				} else if (!account.getCanFollowNow()) {
-					if (lastDateFollowFlowIsManyOneHour(account)) {
+					if (lastDateFollowFlowIsManyInterval(account)) {
 						System.out.println("Passou mais de " + INTERVAL_FOLLOW
 								+ " deis da ultima vez que rodou o flow com a " + account.getNickName());
 						accountFollowFlow(account);
@@ -55,6 +55,8 @@ public class GanharNoInsta {
 	}
 
 	private static void accountFollowFlow(Account account) {
+		Boolean isBlocked = false;
+
 		System.out.println("Logando a " + account.getNickName());
 		Instagram.login(account.getEmail(), account.getPassword());
 
@@ -74,7 +76,6 @@ public class GanharNoInsta {
 		for (int i = 1; i <= QUANTITY_FOLLOWS_IN_INTERVAL_TIME; i++) {
 			new WebDriverWait(Driver.access, 9999999)
 					.until(ExpectedConditions.visibilityOfElementLocated(By.id("btn-acessar")));
-
 			sleep(200);
 
 			WebElement buttonSeeProfile = Driver.access.findElement(By.id("btn-acessar"));
@@ -82,13 +83,11 @@ public class GanharNoInsta {
 
 			while (Driver.access.getWindowHandles().size() > 1) {
 				if (Driver.access.getWindowHandles().size() >= 3) {
-					System.out.print("O perfil da " + account.getNickName() + " não esta podendo seguir no momento.");
-					account.setCanFollowNow(false);
-					account.setLastDateFollowFlow(new Date(new Date().getTime() + INTERVAL_FOLLOW));
+					isBlocked = true;
 				}
 			}
-			
-			if (!account.getCanFollowNow()) {
+
+			if (isBlocked) {
 				break;
 			}
 
@@ -100,8 +99,14 @@ public class GanharNoInsta {
 
 		WebElement buttonStop = Driver.access.findElement(By.id("btn_pausar"));
 
-		account.setLastDateFollowFlow(new Date());
-		account.setCanFollowNow(false);
+		if (isBlocked) {
+			System.out.println("O perfil da " + account.getNickName() + " não esta podendo seguir no momento.");
+			account.setCanFollowNow(false);
+			account.setLastDateFollowFlow(new Date(new Date().getTime() + INTERVAL_FOLLOW));
+		} else {
+			account.setLastDateFollowFlow(new Date());
+			account.setCanFollowNow(false);
+		}
 
 		buttonStop.click();
 		Instagram.logout(10);
@@ -116,7 +121,7 @@ public class GanharNoInsta {
 		}
 	}
 
-	private static boolean lastDateFollowFlowIsManyOneHour(Account account) {
+	private static boolean lastDateFollowFlowIsManyInterval(Account account) {
 		long diferrenceMillisecond = new Date().getTime() - account.getLastDateFollowFlow().getTime();
 
 		return diferrenceMillisecond >= INTERVAL_FOLLOW;
